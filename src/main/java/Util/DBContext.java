3699fa852ed29dd67afdd058f9e16881010698b5
phuchgce181933 <phuchgce181933@gmail.com>
2025-02-19 16:23:04 +0700

    groud4-teamwork

---
+++ src/main/java/Util/DBContext.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Huynh Gia Phuc - CE181933
+ */
+public class DBContext {
+
+    protected Connection conn;
+
+    public DBContext() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String dbURL = "jdbc:sqlserver://localhost:1433;"
+                    +"databaseName=SWP9;"
+                    +"user=sa;"
+                    +"password=123;"
+                    +"encrypt=true;trustServerCertificate=true;";
+            conn = DriverManager.getConnection(dbURL);
+        
+        } catch (SQLException ex) {
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
